d51e587efa7d74da6614fdd425e6d91eed845c3d
MoNo258 <monika258@gmail.com>
2022-04-15 12:05:15 +0200

    plugins for images and updated packages

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,10 +1,10 @@
-import type { GatsbyConfig } from "gatsby"
+import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
-}
+  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+};
 
-export default config
+export default config;
