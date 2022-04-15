0feb929f6573a4c53b4c5477a1161ac61a1c848b
MoNo258 <monika258@gmail.com>
2022-04-15 14:55:17 +0200

    added blog pages - start

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,9 +2,29 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    // siteUrl: `https://www.yourdomain.tld`,
+    title: "Gatsby basics Application",
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+  // plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        defaults: {
+          placeholder: `blurred`,
+          backgroundColor: `transparent`,
+        },
+      },
+    },
+    `gatsby-plugin-image`,
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog/`,
+      },
+    },
+  ],
 };
 
 export default config;
---
+++ src/pages/blog.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/layout";
+
+const BlogPage: React.FC = () => {
+  return (
+    <Layout pageTitle="My Blog Posts">
+      <p>My posts will go in here</p>
+    </Layout>
+  );
+};
+
+export default BlogPage;
