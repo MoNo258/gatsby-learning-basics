feac2ce7c188f6d2980cab54436eb9a3099ccf2d
MoNo258 <monika258@gmail.com>
2022-04-27 14:01:20 +0200

    replacing workaround roe __dirname with path.resolve

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,14 +1,6 @@
 import type { GatsbyConfig } from "gatsby";
 import path from "path";
 
-const cacheFolder = path.join(".cache", "compiled");
-
-const dirnameNew =
-  process.env.NODE_ENV === "production" ||
-  process.env.NODE_ENV === "development"
-    ? __dirname.replace(cacheFolder, "")
-    : __dirname;
-
 const config: GatsbyConfig = {
   siteMetadata: {
     // siteUrl: `https://www.yourdomain.tld`,
@@ -30,7 +22,7 @@ const config: GatsbyConfig = {
       resolve: "gatsby-source-filesystem",
       options: {
         name: `blog`,
-        path: `${dirnameNew}/blog/`,
+        path: path.resolve(`blog`),
       },
     },
     "gatsby-plugin-mdx",
